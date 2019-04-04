8c6ad6b260e6e12c6f66f8a265bf62636f301bbd
wtj900 <wtj900@163.com>
2019-04-04 15:10:00 +0800

    a

--- ZGSpecialKit/Classes/Special(专题)/ZGSpecialKit.h
+++ ZGSpecialKit/Classes/Special(专题)/ZGSpecialKit.h
@@ -19,6 +19,11 @@
 #import <Masonry/Masonry.h>
 #import <YYWebImage/YYWebImage.h>
 #import <ZGReformerKeysKit/ZGReformerKeysKit-umbrella.h>
+#import <ZGAPIsKit/ZGAPIsKit-umbrella.h>
+#import <ZGConfigsKit/ZGConfigsKit-umbrella.h>
+#import <ZGToolsKit/ZGToolsKit-umbrella.h>
+#import <ZGCategoryKit/ZGCategoryKit-umbrella.h>
+#import <ZGMainViewKit/ZGMainViewKit-umbrella.h>
 #import "FFReformProtocol.h"
 
 #endif /* ZGSpecialKit_h */
